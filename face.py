3236e0e683d3e5e9bd4acb54ff037084b46f507e
himansh <hssharma2212000@gmail.com>
2019-10-11 00:41:42 +0530

    face

--- face.py
+++ face.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-img = cv.imread('sachin.jpg')
+img = cv.imread('abc.jfif')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
